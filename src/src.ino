cc29a44ed03e368364e36a92108a9f5367784d46
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-08-15 20:18:01 -0400

    Send mock path data over bluetooth

--- src/src.ino
+++ src/src.ino
@@ -1,14 +1,72 @@
-#include "lcd.h"
+#include <SPI.h>
+#include "Adafruit_BLE_UART.h"
+
+#define REQ 9
+#define RDY 7
+#define RST 10
+
+Adafruit_BLE_UART bluetooth = Adafruit_BLE_UART(REQ, RDY, RST);
 
 void setup(void)
 {
-  lcd_init();
+  Serial.begin(9600);
+  while (!Serial);
+  bluetooth.setDeviceName("Ralph");
 }
 
 void loop(void)
 {
-  lcd_print_double(1.2345, 0) ;
-  lcd_print_double(9.345, 1) ;
-  lcd_print_double(20.20, 4) ;
-  lcd_print_double(58.34, 5) ;
+  if (Serial.available()) {
+    String s = Serial.readString();
+    if (s == "Send") {
+      send();
+    }
+  }
 }
+
+void send(void)
+{
+  Serial.println("Waiting");
+  bluetooth.begin();
+
+  aci_evt_opcode_t status = ACI_EVT_DISCONNECTED;
+
+  /* wait for connect */
+  while (status != ACI_EVT_CONNECTED) {
+    bluetooth.pollACI();
+    status = bluetooth.getState();
+  }
+  Serial.println("Connected");
+
+  /* dump payload */
+  bluetooth.println("start");
+  send_point("43.7", "-70.6", "1.5");
+  send_point("43.6", "-70.6", "1.4");
+  send_point("43.6", "-70.5", "1.3");
+  send_point("43.7", "-70.5", "1.2");
+  send_point("43.7", "-70.6", "1.1");
+  bluetooth.println("finish");
+  Serial.println("Done");
+}
+
+/* for some reason without print_status between each call,
+ * bluetooth.print bugs out. Maybe the call to pollACI fixes */
+void send_point(String latitude, String longitude, String speed)
+{
+  bluetooth.print(latitude);
+  print_status();
+  bluetooth.print(longitude);
+  print_status();
+  bluetooth.print(speed);
+  print_status();
+  delay(1000);
+}
+
+void print_status(void)
+{
+  bluetooth.pollACI();
+  aci_evt_opcode_t status = bluetooth.getState();
+  Serial.println(status);
+}
+
+
