acf41a0cb67690d3c1676f4134fd23c2119e9a2a
Andrew Hayford <andrew.hayford@maine.edu>
2015-09-07 17:59:22 -0400

    Add partially working button interrupt service routines/ debugging code

--- src/src.ino
+++ src/src.ino
@@ -1,15 +1,14 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_GPS.h>
 extern "C" {
-  #include "lcd.h"
-  #include "haversine.h"
+#include "lcd.h"
+#include "haversine.h"
 }
 
 #define GPSSerial Serial1
 #define USBSerial Serial
 
-#define BUT_STOP  3
-#define BUT_STAR  2
+#define BUT_STAR  3
 #define BUT_BLUE  7
 
 #define GPSECHO  true
@@ -19,43 +18,51 @@ extern "C" {
 Adafruit_GPS GPS(&GPSSerial);
 HardwareSerial mySerial = Serial1;
 
+volatile int coutner1 = 0;
+volatile int coutner2 = 0;
+
 void setup(void)
 {
-  pinMode(BUT_STOP, INPUT) ;
   pinMode(BUT_STAR, INPUT) ;
   pinMode(BUT_BLUE, INPUT) ;
-
-  attachInterrupt(1,isr_stop,HIGH) ;
+  
+  attachInterrupt(1, isr_blank, HIGH) ;
   attachInterrupt(0,isr_start,HIGH) ;
   attachInterrupt(4,isr_bluetooth,HIGH);
-  
-  lcd_init();
-  lcd_clear_display();
-  lcd_write_str("init");
-   
-  USBSerial.begin(115200);
-  delay(1000);
 
-  GPS.begin(9600);
-  
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ) ;
-  GPS.sendCommand(PGCMD_ANTENNA);
+  /* lcd_init();
+   lcd_clear_display();
+   lcd_write_str("init");
 
-  delay(1000);
-  GPSSerial.println(PMTK_Q_RELEASE);
-  lcd_clear_display() ;
+   USBSerial.begin(115200);
+   delay(1000);
+
+   GPS.begin(9600);
+
+   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ) ;
+   GPS.sendCommand(PGCMD_ANTENNA);
+
+   delay(1000);
+   GPSSerial.println(PMTK_Q_RELEASE);
+   lcd_clear_display() ; */
 }
 
 void loop(void)
 {
+  delay(1000) ;
+  Serial.print(coutner1) ;
+  Serial.write(":") ;
+  Serial.print(coutner2) ;
+  Serial.write("\n") ;
+  delay(1000);
   /*
   char c = GPS.read();
 
   if ((c) && (GPSECHO)) {
-    USBSerial.write(c); 
+    USBSerial.write(c);
   }
-  
+
   if (GPS.newNMEAreceived()) {
 
     if (GPS.parse(GPS.lastNMEA()) && GPS.fix) {
@@ -79,16 +86,16 @@ void print_GPS_results(void)
   if (first) {
     first = !first ;
   } else {
-    dist_speed += (speed/3600.0)*10.0*5280 ; 
+    dist_speed += (speed / 3600.0) * 10.0 * 5280 ;
     coord_diff = calc_dist_coord(GPS.latitude, GPS.longitude,
-                                    last_lat, last_lng)*M_TO_FT ;
+                                 last_lat, last_lng) * M_TO_FT ;
     dist_coord +=  coord_diff ;
-    speed = (coord_diff * 360.0 )/5280.0 ;
+    speed = (coord_diff * 360.0 ) / 5280.0 ;
   }
-  
+
   last_lat = GPS.latitude ;
   last_lng = GPS.longitude ;
-    
+
   lcd_print_float(dist_coord) ;
   lcd_print_float(speed) ;
   lcd_print_float(GPS.latitude) ;
@@ -97,29 +104,42 @@ void print_GPS_results(void)
 
 void isr_start(void)
 {
-  lcd_pos(0,0) ;
-  lcd_write_str("start") ;
-  delay(3000);              // wait for a second
-  lcd_clear_display() ;
-}
-
-void isr_stop(void)
-{
-  lcd_pos(0,0) ;
-  lcd_write_str("stop") ;
-  delay(3000);              // wait for a second
-  lcd_clear_display() ;
+  if (debounce(3) ) {
+    detachInterrupt(1) ;
+    noInterrupts() ;
+    coutner1++ ;
+    interrupts() ;
+    attachInterrupt(0, isr_start, HIGH) ;
+  }
 }
 
 void isr_bluetooth(void)
 {
-  lcd_pos(0,0) ;
-  lcd_write_str("bluetooth") ;
-  delay(3000);
-  lcd_clear_display() ;
+  if ( debounce(7) ) {
+    detachInterrupt(1) ;
+    noInterrupts() ;
+    coutner2++ ;
+    interrupts() ;
+    attachInterrupt(4, isr_bluetooth, HIGH) ;
+  }
 }
 
-      
-      
-    
+void isr_blank(void)
+{
+  // this function does nothing but it makes shit work 
+  return ;
+}
+
+bool debounce(int pin) 
+{
+  bool safe = 1 ;
+  for (int i = 0; i < 5; i++) {
+     if (digitalRead(pin) != HIGH ) {
+       safe = 0 ;
+       return safe ;
+     }
+  }
+  return safe ;
+}
+
 
