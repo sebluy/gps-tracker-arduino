e1a15327a93be1bb6fb34b013b16a993058ada26
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-10-31 23:36:04 -0400

    Move gps polling into seperate function

--- src/src.ino
+++ src/src.ino
@@ -23,9 +23,7 @@ void setup(void)
   /* Initialise LCD - Print startup message */
   lcd_init();
   lcd_clear_display();
-  lcd_write_str("Cache Rules Everything Around Me");
-  delay(2500) ;
-  lcd_clear_display();
+  lcd_write_str("Waiting for fix...");
   
   /* Wait for serial to be ready - start GPS at 9600 baud */
   GPS.begin(9600);
@@ -42,42 +40,48 @@ void setup(void)
 void loop(void)
 { 
     waypoint_store_t waypoint_store;
+
     point_t waypoint;
     point_t tracking_point;
-    char c;
+
     float distance_to_waypoint;
-    int waypoint_num = 0;
 
     waypoint_store_initialize(&waypoint_store);
     waypoint = waypoint_store_get_next(&waypoint_store);
 
     while (1) {
-        c = GPS.read();
-        if (GPS.newNMEAreceived() && GPS.parse(GPS.lastNMEA()) && GPS.fix) {
-            
-            digitalWrite(BUSY_LED, HIGH); /* turn on LED */
+        gps_wait_for_packet(GPS);
 
-            tracking_point.latitude = GPS.latitudeDegrees;
-            tracking_point.longitude = GPS.longitudeDegrees;
-            
-            distance_to_waypoint = distance_between(waypoint, tracking_point);
+        digitalWrite(BUSY_LED, HIGH); /* turn on LED */
 
-            if (distance_to_waypoint < WAYPOINT_DISTANCE_THRESHOLD) {
-                if (waypoint_store_end(&waypoint_store)) {
-                    /* loop forever if out of waypoints */
-                    lcd_write_str("Path Complete");
-                    while (1);
-                } else {
-                    /* else move on to next waypoint */
-                    waypoint = waypoint_store_get_next(&waypoint_store);
-                    distance_to_waypoint = distance_between(waypoint, tracking_point);
-                }
+        tracking_point.latitude = GPS.latitudeDegrees;
+        tracking_point.longitude = GPS.longitudeDegrees;
+            
+        distance_to_waypoint = distance_between(waypoint, tracking_point);
+
+        if (distance_to_waypoint < WAYPOINT_DISTANCE_THRESHOLD) {
+            if (waypoint_store_end(&waypoint_store)) {
+                /* loop forever if out of waypoints */
+                lcd_write_str("Path Complete");
+                while (1);
+            } else {
+                /* else move on to next waypoint */
+                waypoint = waypoint_store_get_next(&waypoint_store);
+                distance_to_waypoint = distance_between(waypoint, tracking_point);
             }
-
-            print_distance_to_waypoint(distance_to_waypoint);
-
-            digitalWrite(BUSY_LED, LOW); /* turn off LED */
         }
+
+        print_distance_to_waypoint(distance_to_waypoint);
+
+        digitalWrite(BUSY_LED, LOW); /* turn off LED */
+    }
+}
+
+void gps_wait_for_packet(Adafruit_GPS gps)
+{
+    /* wait until a packet arrives with a fix */
+    while (!(GPS.newNMEAreceived() && GPS.parse(GPS.lastNMEA()) && GPS.fix)) {
+        GPS.read();
     }
 }
 
