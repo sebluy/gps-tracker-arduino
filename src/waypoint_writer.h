5d4364e3e002795ba090b60cfc7ba3aba8e93d72
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-11-15 22:51:05 -0500

    Cleanup waypoint writer

--- src/waypoint_writer.h
+++ src/waypoint_writer.h
@@ -1,6 +1,11 @@
 #ifndef WAYPOINT_WRITER_H
 #define WAYPOINT_WRITER_H
 
+/* A waypoint writer is used for storing waypoints in non-volatile storage.
+   Only one waypoint path can be stored at any given time.
+   Creating a new waypoint_writer and writing to it will overwrite the previous
+   waypoint path. */
+
 enum waypoint_field_t {COUNT, LATITUDE, LONGITUDE};
 
 struct waypoint_writer_t {
@@ -10,9 +15,9 @@ struct waypoint_writer_t {
 
 void waypoint_writer_initialize(waypoint_writer_t *writer);
 
-/* Writes a waypoint field to storage. After initialization should
+/* Writes a waypoint field to storage. After initialization, should
    be called successively with count, lat1, lng1, lat2, lng2, ...
-   for "count" points */
+   for "count" points. */
 void waypoint_writer_write(waypoint_writer_t *writer, char *field);
 
 #endif
